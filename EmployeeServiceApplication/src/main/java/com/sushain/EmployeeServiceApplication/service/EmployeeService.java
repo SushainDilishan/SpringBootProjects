3a90a82f2dc322926ea99c15d331167e0b0d1680
SushainDilishan <Sushaind96@gmail.com>
2020-02-08 22:35:44 +0530

    asa

---
+++ EmployeeServiceApplication/src/main/java/com/sushain/EmployeeServiceApplication/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.sushain.EmployeeServiceApplication.service;
+
+import com.sushain.EmployeeServiceApplication.model.Employee;
+
+public interface EmployeeService {
+
+    Employee save(Employee e);
+}
