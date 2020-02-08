5d7fdd5cead564cd2e2f11dbd6c977a8ae07deb1
SushainDilishan <Sushaind96@gmail.com>
2020-02-08 22:36:36 +0530

    aaaa

--- EmployeeServiceApplication/src/main/java/com/sushain/EmployeeServiceApplication/model/Employee.java
+++ EmployeeServiceApplication/src/main/java/com/sushain/EmployeeServiceApplication/model/Employee.java
@@ -1,14 +1,37 @@
 package com.sushain.EmployeeServiceApplication.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.List;
-
+@Entity
 public class Employee {
+
+    @Id
+    Integer id;
     String name;
     String location;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String name, String location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -26,7 +49,7 @@ public class Employee {
         this.location = location;
     }
 
-    public static List<Employee> getAllEmployees(){
+    public  List<Employee> listEmployees(){
 
         Employee employee =new Employee();
         List<Employee> employees = new ArrayList<>();
