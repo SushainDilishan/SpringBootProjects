f2016e25d389938386588cfcdd2866f73e875b6a
SushainDilishan <Sushaind96@gmail.com>
2020-02-16 14:55:19 +0530

    aa

--- SpringWithAllocation/AllocationService/src/main/java/com/sushain/AllocationServiceApplication/Repository/AllocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sushain.AllocationServiceApplication.Repository;
-
-import com.sushain.AllocationServiceApplication.Model.Allocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AllocationRepository extends JpaRepository<Allocation,Integer> {
-
-}
-
--- SpringWithAllocation/EmployeeServiceApplication/src/main/java/com/sushain/EmployeeServiceApplication/Repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sushain.EmployeeServiceApplication.Repository;
-
-import com.sushain.EmployeeServiceApplication.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-}
